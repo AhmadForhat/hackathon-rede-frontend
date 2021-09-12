a7ab95a16efd3d2a23ce820482fe3ca8beff0eec
guscarpim <gustavoscarpim@gmail.com>
2021-09-12 00:51:02 -0300

    add: Perfil

--- src/pages/Perfil/Perfil.tsx
+++ src/pages/Perfil/Perfil.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { Flag, Info, Settings, LogOut } from "react-feather";
+import Pedro from "./pedro.png";
+
+import {
+  Container,
+  ContentPerfil,
+  Article,
+  Title,
+  Subtitle,
+  Img,
+  Section,
+  ButtonInfos,
+  ButtonLogout,
+  ContentButton,
+} from "./styles";
 
 const Perfil: React.FC = () => {
   return (
-    <div>
+    <Container>
+      <ContentPerfil>
+        <Article>
+          <Title>Pedro</Title>
+          <Subtitle>Soluções de contorno</Subtitle>
+        </Article>
 
-    </div>
-  )
-}
+        <Img src={Pedro} alt="Usuário" />
+      </ContentPerfil>
 
-export default Perfil
+      <Section>
+        <ButtonInfos background="#FFB08E" color="#FF5B14">
+          <Flag /> Reportes
+        </ButtonInfos>
+        <ButtonInfos background="#96C0FF" color="#1447A8">
+          <Info /> Informações
+        </ButtonInfos>
+        <ButtonInfos background="#3A9258" color="#A2F0BD">
+          <Settings /> Configurações
+        </ButtonInfos>
+      </Section>
+
+      <ContentButton>
+        <ButtonLogout>
+          <LogOut /> Deslogar
+        </ButtonLogout>
+      </ContentButton>
+    </Container>
+  );
+};
+
+export default Perfil;
