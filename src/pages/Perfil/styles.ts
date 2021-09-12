8351b47faac8e929a0efb1cde307c36ba55a8e47
guscarpim <gustavoscarpim@gmail.com>
2021-09-12 00:48:55 -0300

    refactor: Adicionando cores do theme

---
+++ src/pages/Perfil/styles.ts
@@ -0,0 +1,136 @@
+import styled from 'styled-components';
+
+interface Ibutton {
+  background: string;
+  color: string;
+}
+
+export const Container = styled.div`
+  padding: 30px 20px;
+`
+
+export const ContentPerfil = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const Title = styled.h1`
+  font: normal normal bold 24px 'Poppins', sans-serif;
+  margin: 0;
+  color: ${({ theme }) => theme.colors.ink};
+`
+
+export const Subtitle = styled.span`
+  color: ${({ theme }) => theme.colors.gray};
+  font: normal normal normal 22px 'Poppins', sans-serif;
+  display: -webkit-box;
+  max-width: 225px;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+export const Img = styled.img`
+  width: 74px;
+  height: auto;
+  object-fit: contain;
+  border: 2px solid ${({ theme }) => theme.colors.secondary};
+  border-radius: 100%;
+  padding: 3.5px;
+`
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 50px;
+`
+
+export const ButtonInfos = styled.button<Ibutton>`
+  border: none;
+  background: none;
+  margin-top: 10px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  font: normal normal bold 18px 'Poppins', sans-serif;
+  position: relative;
+  border-radius: 10px;
+  transition: all 0.5s;
+
+  svg {
+    background: ${props => props.background && props.background};
+    color: ${props => props.color && props.color};
+    border-radius: 100%;
+    padding: 7px;
+    width: 18px;
+    height: 18px;
+    margin-right: 14px;
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.ice};
+  }
+
+  &::before {
+    position: absolute;
+    content: '';
+    right: 10px;
+    border-bottom: 2px solid;
+    height: 12px;
+    width: 12px;
+    transform: rotate(-45deg);
+  }
+
+  &::after {
+    position: absolute;
+    content: '';
+    right: 10px;
+    border-bottom: 2px solid;
+    height: 12px;
+    width: 12px;
+    transform: rotate(-125deg);
+  }
+`
+
+export const ContentButton = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 50px;
+`
+
+export const ButtonLogout = styled.button`
+  width: 140px;
+  height: 40px;
+  font: normal normal bold 18px 'Poppins', sans-serif;
+  color: ${({ theme }) => theme.colors.inkLighter};
+  border-radius: 8px;
+  padding: 9px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background: ${({theme}) => theme.colors.ice};
+  transition: all 0.3s;
+
+  svg {
+    margin-right: 10px;
+    width: 20px;
+    color: ${({ theme }) => theme.colors.inkLighter};
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primaryDarker};
+    svg {
+      color: ${({ theme }) => theme.colors.primaryDarker};
+    }
+  }
+`
