66cf8f88f63eb91b2430a385def3f776418f1215
chrigor <chrigor.edu3@gmail.com>
2021-09-11 22:49:15 -0300

    refactors dos inputs

--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -1,19 +1,59 @@
+import React, { useRef } from 'react';
+
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+
 import CadastroSvg from './cadastro.svg';
+
+import getValidationErrors from '../../utils/getValidationsErrors';
+
+import LabelInput from '../../components/atoms/LabelInput';
+import InputLine from '../../components/atoms/InputLine';
 
 import {
   Container,
   Logo,
   Card,
   Title,
-  Form,
+  FormCustom,
   ContentInput,
-  Label,
-  Input,
   LabelMin,
   Button
 } from "../Login/styles";
 
 const Cadastro: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+
+  const handleFormSubmit = async (data: any) => {
+
+    try {
+      formRef.current && formRef.current.setErrors({});
+      console.log(data)
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Digite o Email')
+          .email('Digite um email válido'),
+        password: Yup.string().required('Digite a senha'),
+        passwordConfirm: Yup.string().required('Digite a confirmação de senha'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+    } catch (err) {
+
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current && formRef.current.setErrors(errors);
+        return;
+      }
+
+    }
+  }
+
   return (
     <Container>
       <Logo src={CadastroSvg} alt="cadastro" />
@@ -21,26 +61,26 @@ const Cadastro: React.FC = () => {
       <Card>
         <Title>Criar conta</Title>
 
-        <Form>
+        <FormCustom ref={formRef} onSubmit={handleFormSubmit}>
           <ContentInput>
-            <Label>Email</Label>
-            <Input required type="email" placeholder="Digite seu email" />
+            <LabelInput>Email</LabelInput>
+            <InputLine type="email" name="email" placeholder="Digite seu email" />
           </ContentInput>
 
           <ContentInput>
-            <Label>Senha</Label>
-            <Input required type="password" placeholder="Digite sua senha" />
+            <LabelInput>Senha</LabelInput>
+            <InputLine type="password" name="password" placeholder="Digite sua senha" />
           </ContentInput>
 
           <ContentInput>
-            <Label>Confirmar senha</Label>
-            <Input required type="password" placeholder="Digite sua senha" />
+            <LabelInput>Confirmar senha</LabelInput>
+            <InputLine type="password" name="passwordConfirm" placeholder="Digite sua senha" />
           </ContentInput>
 
           <Button type="submit">Cadastrar</Button>
 
           <LabelMin to="/">Já possui uma conta?</LabelMin>
-        </Form>
+        </FormCustom>
       </Card>
     </Container>
   );
