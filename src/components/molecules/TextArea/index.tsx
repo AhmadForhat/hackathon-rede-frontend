2d72730213df9a4b048c3f9de68951b19d14f5b5
guscarpim <gustavoscarpim@gmail.com>
2021-09-12 16:07:56 -0300

    refactor: Add novas props e identacao

--- src/components/molecules/TextArea/index.tsx
+++ src/components/molecules/TextArea/index.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Textarea from 'components/atoms/TextArea';
-import LabelInput from 'components/atoms/LabelInput';
+import Textarea from "components/atoms/TextArea";
+import LabelInput from "components/atoms/LabelInput";
 
-interface TextAreaProps  {
-    label?: string
-    name: string,
-    rows?: number
+interface TextAreaProps {
+  label?: string;
+  name: string;
+  rows?: number;
+  placeholder?: string;
 }
 
-const TextAreaCustom: React.FC<TextAreaProps> = ({ label, name, ...props }) => {
-    return (
-        <div>
-            {label && (
-                <LabelInput htmlFor={name}>
-                    {label}
-                </LabelInput>
-            )}
-            <Textarea name={name} {...props }/>
-        </div>)
-}
+const TextAreaCustom: React.FC<TextAreaProps> = ({ label, name, placeholder, ...props }) => {
+  return (
+    <div>
+      {label && <LabelInput htmlFor={name}>{label}</LabelInput>}
+      <Textarea name={name} placeholder={placeholder} {...props} />
+    </div>
+  );
+};
 
 export default TextAreaCustom;
