ed374e1fd8746f003c0e5ba2ce7708853e2c2682
guscarpim <gustavoscarpim@gmail.com>
2021-09-12 02:38:10 -0300

    Estilos

---
+++ src/components/atoms/CardReports/styles.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Card = styled.div`
+  border-radius: 10px;
+  margin-top: 20px;
+  min-height: 106px;
+  max-height: 106px;
+  width: 98%;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 0px 0px 2px rgba(0, 0, 0, 0.1);
+`
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.ink};
+  font: normal normal 700 15px 'Poppins', sans-serif;
+  text-align: start;
+  margin: 15px 15px 0;
+  display: block;
+  display: -webkit-box;
+  max-width: 98%;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+export const ContentInfosDate = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`
+
+export const DescriptionDate = styled.span`
+  display: flex;
+  align-items: center;
+  margin: 10px 15px 0;
+  font: normal normal 700 14px 'Poppins', sans-serif;
+  color: ${({ theme }) => theme.colors.gray};
+  width: 100%;
+
+  svg {
+    margin-right: 8px;
+    width: 14px;
+    height: 14px;
+  }
+`
+
+export const ContentInfos = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 22px 15px 0;
+`
+
+export const Comments = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.ink};
+  font: normal normal 700 14px 'Roboto', sans-serif;
+
+  svg {
+    margin-right: 6px;
+    width: 16px;
+    height: 16px;
+  }
+`
+
+export const Anexts = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    color: ${({ theme }) => theme.colors.gray};
+    margin-right: 8px;
+    width: 16px;
+    height: 16px;
+  }
+`
